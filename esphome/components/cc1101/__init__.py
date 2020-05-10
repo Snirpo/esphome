0bb8c7bbcb68b34d7ddc9377475fe368b48b5b09
Chiel Prins <snirpo@gmail.com>
2020-05-10 23:50:25 +0200

    added c1101 device register

--- esphome/components/cc1101/__init__.py
+++ esphome/components/cc1101/__init__.py
@@ -1,7 +1,8 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import spi
-from esphome.const import CONF_ID
+from esphome.const import CONF_ID, CONF_CC1101_ID
+from esphome.core import coroutine
 
 DEPENDENCIES = ['spi']
 MULTI_CONF = False
@@ -12,6 +13,16 @@ CC1101Component = cc1101_ns.class_('CC1101Component', cg.Component, spi.SPIDevic
 CONFIG_SCHEMA = cv.All(cv.Schema({
     cv.GenerateID(): cv.declare_id(CC1101Component),
 }).extend(cv.COMPONENT_SCHEMA).extend(spi.SPI_DEVICE_SCHEMA))
+
+C1101_DEVICE_SCHEMA = cv.Schema({
+    cv.GenerateID(CONF_CC1101_ID): cv.use_id(CC1101Component),
+})
+
+
+@coroutine
+def register_c1101_device(var, config):
+    parent = yield cg.get_variable(config[CONF_CC1101_ID])
+    cg.add(var.set_c1101_parent(parent))
 
 
 def to_code(config):
