2bd977896da2decb96cbf24c3479ef5234480c58
Chiel Prins <snirpo@gmail.com>
2020-05-21 00:10:41 +0200

    WIP

--- esphome/components/kaku_cc1101/kaku_cc1101.cpp
+++ esphome/components/kaku_cc1101/kaku_cc1101.cpp
@@ -5,25 +5,9 @@
 namespace esphome {
 namespace kaku_cc1101 {
 
-static const char *TAG = "itho_ecofanrft";
-
-std::string KakuCC1101Component::format_addr_(std::vector<uint8_t> addr) {
-  std::string s;
-  char buf[20];
-  for (uint8_t a : addr) {
-    sprintf(&buf[0], "%02X:", a);
-    s += buf;
-  }
-  s.pop_back();
-  return s;
-}
+static const char *TAG = "kaku_cc1101";
 
 void KakuCC1101Component::dump_config() {
-  std::string s;
-
-  // ESP_LOGCONFIG(TAG, "Itho EcoFanRft '%s'", this->get_name().c_str());
-  ESP_LOGCONFIG(TAG, "  RF Address: '%s'", this->format_addr_(this->rf_address_).c_str());
-
 #ifdef ESPHOME_LOG_HAS_VERY_VERBOSE
   std::vector<uint8_t> config = this->cc1101_->read_burst_register(0x00, 47);
   for (uint8_t i = 0; i < config.size(); i++) {
@@ -32,156 +16,85 @@ void KakuCC1101Component::dump_config() {
 #endif
 }
 void KakuCC1101Component::setup() {
-  if (!this->cc1101_->is_failed()) {
-    this->mark_failed();
-    return;
-  }
-
-  // Setup module for Itho protocol
-  this->init_kaku();
-}
-void KakuCC1101Component::loop() {}
-
-float KakuCC1101Component::get_setup_priority() const { return setup_priority::DATA; }
-
-void KakuCC1101Component::send_command(std::string command) {
-  this->prepare_packet(command);
-  this->send_packet();
-
-  this->set_timeout("send_command", 40, [this]() { this->schedule_send_packet_(); });
-}
-
-void KakuCC1101Component::schedule_send_packet_() {
-  uint8_t tries_left = this->send_packet();
-
-  if (tries_left > 0) {
-    this->set_timeout("send_command", 50, [this]() { this->schedule_send_packet_(); });
-  }
-}
-
-void KakuCC1101Component::init_kaku() {
   for (register_setting r : KAKU_CONFIG) {
     this->cc1101_->write_register(r.address, r.data);
   }
   this->cc1101_->write_burst_register(CC1101_PATABLE, KAKU_PATABLE);
 
   this->cc1101_->write_command_strobe(CC1101_SCAL);
+
+  this->cc1101_->send();
   delay(1);
 }
 
-uint8_t KakuCC1101Component::calc_checksum(std::vector<uint8_t> data) {
-  uint8_t checksum = 0;
-  for (unsigned char i : data) {
-    checksum = (checksum + i) & 0xFF;
-  }
-  return 0 - checksum;
-}
-
-void KakuCC1101Component::prepare_packet(std::string command) {}
+float KakuCC1101Component::get_setup_priority() const { return setup_priority::DATA; }
 
 uint8_t KakuCC1101Component::send_packet() {
   ESP_LOGV(TAG, "%d tries left for sending packet of %d bytes", this->tries_, this->packet_.size());
-  if (this->packet_.size() > 0 && this->tries_-- > 0) {
+  if (!this->packet_.empty() && this->tries_-- > 0) {
     this->cc1101_->send_data(this->packet_);
   }
 
-  return this->tries_;
+  if (tries_ > 0) {
+    this->set_timeout("send_packet", 50, [this]() { this->send_packet(); });
+  }
 }
 
-bool KakuCC1101Component::receiveProtocol(const int p, unsigned int changeCount) {
-  const Protocol &pro = proto[p - 1];
-
-  unsigned long code = 0;
-  // Assuming the longer pulse length is the pulse captured in timings[0]
-  const unsigned int syncLengthInPulses =
-      ((pro.syncFactor.low) > (pro.syncFactor.high)) ? (pro.syncFactor.low) : (pro.syncFactor.high);
-  const unsigned int delay = KakuCC1101Component::timings[0] / syncLengthInPulses;
-  const unsigned int delayTolerance = delay * KakuCC1101Component::nReceiveTolerance / 100;
-
-  /* For protocols that start low, the sync period looks like
-   *               _________
-   * _____________|         |XXXXXXXXXXXX|
-   *
-   * |--1st dur--|-2nd dur-|-Start data-|
-   *
-   * The 3rd saved duration starts the data.
-   *
-   * For protocols that start high, the sync period looks like
-   *
-   *  ______________
-   * |              |____________|XXXXXXXXXXXXX|
-   *
-   * |-filtered out-|--1st dur--|--Start data--|
-   *
-   * The 2nd saved duration starts the data
-   */
-  const unsigned int firstDataTiming = (pro.invertedSignal) ? (2) : (1);
-
-  for (unsigned int i = firstDataTiming; i < changeCount - 1; i += 2) {
-    code <<= 1;
-    if (diff(KakuCC1101Component::timings[i], delay * pro.zero.high) < delayTolerance &&
-        diff(KakuCC1101Component::timings[i + 1], delay * pro.zero.low) < delayTolerance) {
-      // zero
-    } else if (diff(KakuCC1101Component::timings[i], delay * pro.one.high) < delayTolerance &&
-               diff(KakuCC1101Component::timings[i + 1], delay * pro.one.low) < delayTolerance) {
-      // one
-      code |= 1;
-    } else {
-      // Failed
-      return false;
-    }
-  }
-
-  if (changeCount > 7) {  // ignore very short transmissions: no device sends them, so this must be noise
-    KakuCC1101Component::nReceivedValue = code;
-    KakuCC1101Component::nReceivedBitlength = (changeCount - 1) / 2;
-    KakuCC1101Component::nReceivedDelay = delay;
-    KakuCC1101Component::nReceivedProtocol = p;
-    return true;
-  }
-
-  return false;
+light::LightTraits KakuCC1101Component::get_traits() {
+  auto traits = light::LightTraits();
+  traits.set_supports_brightness(true);
+  return traits;
 }
 
-void KakuCC1101Component::handleInterrupt() {
-  static unsigned int changeCount = 0;
-  static unsigned long lastTime = 0;
-  static unsigned int repeatCount = 0;
+void KakuCC1101Component::write_state(light::LightState *state) {
+  bool manchester = true;
 
-  const long time = micros();
-  const unsigned int duration = time - lastTime;
+  packet_.clear();
 
-  if (duration > KakuCC1101Component::nSeparationLimit) {
-    // A long stretch without signal level change occurred. This could
-    // be the gap between two transmission.
-    if ((repeatCount == 0) || (diff(duration, KakuCC1101Component::timings[0]) < 200)) {
-      // This long signal is close in length to the long signal which
-      // started the previously recorded timings; this suggests that
-      // it may indeed by a a gap between two transmissions (we assume
-      // here that a sender will send the signal multiple times,
-      // with roughly the same gap between them).
-      repeatCount++;
-      if (repeatCount == 2) {
-        for (unsigned int i = 1; i <= numProto; i++) {
-          if (receiveProtocol(i, changeCount)) {
-            // receive succeeded for protocol i
-            break;
-          }
-        }
-        repeatCount = 0;
+  packet_.push_back(high);
+  for (uint8_t i = 0; i < 10; i++) {
+    packet_.push_back(low);
+  }
+
+  if (manchester) {
+    //uint8_t high = 0b00000010; // 1 high, 1 low, length = 2
+    //uint8_t low = 0b00100000; // 1 high, 5 low, length = 6
+
+    // Manchester encoded
+    uint8_t high = 0b10100000; // 1 high, 1 low, length = 2
+    uint8_t low = 0b10000010; // 1 high, 5 low, length = 6
+    uint8_t dim = 0b1010;
+
+    uint64_t out = 0;
+
+    for (uint8_t i = 25; i >= 0; i--) {
+      if ((address >> i) & 1) {
+        packet_.push_back(high);
+        packet_.push_back(low);
+      }
+      else {
+        packet_.push_back(low);
+        packet_.push_back(high);
       }
     }
-    changeCount = 0;
   }
+  else {
+    uint8_t low = 0b10100000;
+    uint8_t high = 0b10000010;
+    uint8_t dim = 0b1010;
 
-  // detect overflow
-  if (changeCount >= RCSWITCH_MAX_CHANGES) {
-    changeCount = 0;
-    repeatCount = 0;
+    for (uint8_t i = 25; i >= 0; i--) {
+      packet_.push_back((address >> i) & 1 ? high : low);
+    }
+
+    packet_.push_back(low);                                         // group number
+    packet_.push_back(state->current_values.is_on() ? high : low);  // on/off/dim
+
+    uint8_t light = 0;
+    for (uint8_t i = 3; i >= 0; i--) {
+      packet_.push_back((light >> i) | 1 ? high : low);
+    }
   }
-
-  KakuCC1101Component::timings[changeCount++] = duration;
-  lastTime = time;
 }
 
 }  // namespace kaku_cc1101
--- esphome/components/kaku_cc1101/kaku_cc1101.h
+++ esphome/components/kaku_cc1101/kaku_cc1101.h
@@ -1,117 +1,31 @@
 #pragma once
 
+#include <esphome/components/light/light_output.h>
 #include "esphome/core/component.h"
 #include "esphome/core/automation.h"
 #include "esphome/components/fan/fan_state.h"
 #include "esphome/components/cc1101/cc1101.h"
-#define RCSWITCH_MAX_CHANGES 67
 
 namespace esphome {
 namespace kaku_cc1101 {
 
-/**
- * Description of a single pule, which consists of a high signal
- * whose duration is "high" times the base pulse length, followed
- * by a low signal lasting "low" times the base pulse length.
- * Thus, the pulse overall lasts (high+low)*pulseLength
- */
-struct HighLow {
-  uint8_t high;
-  uint8_t low;
-};
-
-/**
- * A "protocol" describes how zero and one bits are encoded into high/low
- * pulses.
- */
-struct Protocol {
-  /** base pulse length in microseconds, e.g. 350 */
-  uint16_t pulseLength;
-
-  HighLow syncFactor;
-  HighLow zero;
-  HighLow one;
-
-  /**
-   * If true, interchange high and low logic levels in all transmissions.
-   *
-   * By default, RCSwitch assumes that any signals it sends or receives
-   * can be broken down into pulses which start with a high signal level,
-   * followed by a a low signal level. This is e.g. the case for the
-   * popular PT 2260 encoder chip, and thus many switches out there.
-   *
-   * But some devices do it the other way around, and start with a low
-   * signal level, followed by a high signal level, e.g. the HT6P20B. To
-   * accommodate this, one can set invertedSignal to true, which causes
-   * RCSwitch to change how it interprets any HighLow struct FOO: It will
-   * then assume transmissions start with a low signal lasting
-   * FOO.high*pulseLength microseconds, followed by a high signal lasting
-   * FOO.low*pulseLength microseconds.
-   */
-  bool invertedSignal;
-};
-
-class KakuCC1101Component : public esphome::Component, public cc1101::CC1101Device {
+class KakuCC1101Component : public Component, public light::LightOutput, public cc1101::CC1101Device {
  public:
   void setup() override;
-  void loop() override;
   void dump_config() override;
   float get_setup_priority() const override;
 
-  void set_rf_address(uint64_t address) {
-    this->rf_address_.resize(3, 0);
-    for (uint8_t i = 0; i < 3; i++) {
-      this->rf_address_[i] = (address >> (8 * (2 - i))) & 0xff;
-    }
-  }
+  light::LightTraits get_traits() override;
+  void write_state(light::LightState *state) override;
 
-  void join();
-  void send_command(std::string command);
+  void set_address(uint32_t a) { address = a; };
 
  private:
-  constexpr static Protocol proto[] = {
-      {350, {1, 31}, {1, 3}, {3, 1}, false},     // protocol 1
-      {650, {1, 10}, {1, 2}, {2, 1}, false},     // protocol 2
-      {100, {30, 71}, {4, 11}, {9, 6}, false},   // protocol 3
-      {380, {1, 6}, {1, 3}, {3, 1}, false},      // protocol 4
-      {500, {6, 14}, {1, 2}, {2, 1}, false},     // protocol 5
-      {450, {23, 1}, {1, 2}, {2, 1}, true},      // protocol 6 (HT6P20B)
-      {150, {2, 62}, {1, 6}, {6, 1}, false},     // protocol 7 (HS2303-PT, i. e. used in AUKEY Remote)
-      {200, {3, 130}, {7, 16}, {3, 16}, false},  // protocol 8 Conrad RS-200 RX
-      {200, {130, 7}, {16, 7}, {16, 3}, true},   // protocol 9 Conrad RS-200 TX
-      {365, {18, 1}, {3, 1}, {1, 3}, true},      // protocol 10 (1ByOne Doorbell)
-      {270, {36, 1}, {1, 2}, {2, 1}, true},      // protocol 11 (HT12E)
-      {320, {36, 1}, {1, 2}, {2, 1}, true}       // protocol 12 (SM5212)
-  };
-  constexpr static uint8_t numProto = sizeof(proto) / sizeof(proto[0]);
-
-  static int nReceiveTolerance;
-  volatile static unsigned long nReceivedValue;
-  volatile static unsigned int nReceivedBitlength;
-  volatile static unsigned int nReceivedDelay;
-  volatile static unsigned int nReceivedProtocol;
-  const static unsigned int nSeparationLimit;
-  /*
-   * timings[0] contains sync timing, followed by a number of bits
-   */
-  static unsigned int timings[RCSWITCH_MAX_CHANGES];
-
-  std::vector<uint8_t> rf_address_;
-  bool next_update_{true};
-  std::vector<uint8_t> packet_;
-  std::uint8_t counter_ = 0;
+  uint32_t address;
+  std::vector<uint8> packet_;
   std::uint8_t tries_ = 0;
 
-  static inline unsigned int diff(int A, int B) { return abs(A - B); }
-  std::string format_addr_(std::vector<uint8_t> addr);
-  void schedule_send_packet_();
-  void init_kaku();
-  uint8_t calc_checksum(std::vector<uint8_t> data);
   uint8_t send_packet();
-  void prepare_packet(std::string command);
-
-  static void handleInterrupt();
-  static bool receiveProtocol(const int p, unsigned int changeCount);
 };
 
 }  // namespace kaku_cc1101
